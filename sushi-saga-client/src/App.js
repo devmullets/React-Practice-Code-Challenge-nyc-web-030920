ce1cbeec0469f2ae1be1ef764d00d3123ceebf30
devmullets <devmullets@gmail.com>
2020-05-18 12:18:26 -0400

    core deliverables

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,13 +5,77 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
 class App extends Component {
+  state = {
+    sushiList: [],
+    currentPage: 1,
+    budget: 50,
+    eatenSushi: []
+
+  }
+
+  getSushi = () => {
+    fetch(API + `?_page=${this.state.currentPage}&_limit=4`)
+      .then(response => response.json())
+      .then(sushiList => this.setState({ sushiList }))
+
+  }
+
+  eatSushi = (singleSushi, index) => {
+    if (this.state.budget - singleSushi.price > 0){
+      this.chargeCustomer(singleSushi)
+      let updatedImage = singleSushi
+      updatedImage.img_url = ''
+      this.setState({sushiList: [...this.state.sushiList.slice(0, index), updatedImage, ...this.state.sushiList.slice(index + 1)]})
+
+      const oldEats = this.state.eatenSushi
+      this.setState({eatenSushi: [...this.state.eatenSushi, index]})
+
+
+
+    } else {
+      alert('no money homie')
+    }
+    
+    // this.setState({...sushiList, {sushiList[index]}: singleSushi.img_url: ''})
+    // https://stackoverflow.com/questions/45673783/replace-array-entry-with-spread-syntax-in-one-line-of-code
+  }
+
+  chargeCustomer = (sushi) => {
+    let newBudget = this.state.budget - sushi.price
+    this.setState({budget: newBudget})
+  }
+
+  componentDidMount() {
+    this.getSushi()
+    // feels hacky to do this, but once page loads w/page 1 - update state so next click is page 2
+    let x = this.state.currentPage
+    this.setState({ currentPage: x + 1})
+  }
+
+  moreSushi = () => {
+    let x = this.state.currentPage
+    if (x === 25) {
+      this.setState({ currentPage: 1})
+    } else {
+      this.setState({ currentPage: x + 1})
+    }
+    this.getSushi()
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          sushiList={this.state.sushiList}
+          moreSushi={this.moreSushi}
+          eatSushi={this.eatSushi}
+        />
+        <Table 
+          budget={this.state.budget}
+          eatenSushi={this.state.eatenSushi}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi';
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          /*  Render Sushi components here! */
+          props.sushiList.map((singleSushi, index) => <Sushi 
+            key={singleSushi.id}
+            sushi={singleSushi}
+            index={index}
+            eatSushi={props.eatSushi}
+
+          />)
         }
-        <MoreButton />
+        <MoreButton moreSushi={props.moreSushi}/>
       </div>
     </Fragment>
   )
